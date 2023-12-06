6488c1504996cf1be216ce8b307873523127aa09
tofa93 <tofig.valiyev1993@gmail.com>
2023-12-06 02:38:06 +0100

    added few more lines of code

--- server.js
+++ server.js
@@ -3,11 +3,22 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    console.log("request made")
-    // set header content type
     res.setHeader('Content-Type', 'text/html');
-    // send an html file
-    fs.readFile('./views/index.html', (err, data) => {
+
+    let path = './views/';
+    switch (req.url) {
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
+
+    fs.readFile(path, (err, data) => {
         if (err) {
             console.log(err);
             res.end();
