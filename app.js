049ee697929988f1bd4c0e984964c4294fe5c59c
tofa93 <tofig.valiyev1993@gmail.com>
2023-12-14 02:33:32 +0100

    adding third party middleware

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 // register view engine
 app.set('view engine', 'ejs');
 app.listen(3000);
 
+app.use(morgan('dev'));
 
 app.get("/", (req, res) => {
     const blogs = [
