7e75c90d8281fec9ac391dc3c4f29403012d97e5
tofa93 <tofig.valiyev1993@gmail.com>
2023-12-17 00:37:20 +0100

    displaying data on index.ejs

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const blog = require('./models/blog');
-const app = express();
+const Blog = require('./models/blog');
 
-const username = encodeURIComponent("tofa993");
-const password = encodeURIComponent("19932005Vvt");
+const app = express();
 
 const dburi = `mongodb+srv://tofa993:19932005Vvt@cluster0.yu23f4v.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.connect(dburi, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -16,39 +15,26 @@ app.set('view engine', 'ejs');
 
 // middleware & static files(e.g images, css files etc)
 app.use(express.static('public'));
-
 app.use(morgan('dev'));
 
-// sandbox routes for just testing
 
-app.get('/add-blog', (req, res) => {
-    const newBlog = new blog({
-        title: 'New blog 2',
-        snippet: 'About my new blog',
-        body: 'Body text of the blog'
-    });
-
-    newBlog.save()
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-
+// Routes ////////////////////////////////////////////////////////////////////////////
 app.get("/", (req, res) => {
-    const blogs = [
-        { title: 'Blog one', snippet: 'snippet generated' },
-        { title: 'Blog two', snippet: 'snippet generated' },
-        { title: 'Blog three', snippet: 'snippet generated' }
-    ];
-    res.render('index', { title: 'Home', blogz: blogs });
+    res.redirect('/blogs');
 });
 
 app.get("/about", (req, res) => {
     res.render('about', { title: 'About Us' });
+});
+
+app.get("/blogs", (req, res) => {
+    Blog.find().sort({ createdAt: -1 }) // sorting makes the newest blog to appear at the top
+        .then((data) => {
+            res.render('index', { title: 'All Blogs here', blogs: data })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 });
 
 app.get('/blogs/create', (req, res) => {
@@ -58,4 +44,5 @@ app.get('/blogs/create', (req, res) => {
 app.use((req, res) => {
     res.status(404).render('404', { title: '404 Page' });
 });
+// Routes ////////////////////////////////////////////////////////////////////////////
 
