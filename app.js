4b353fed0a1df24a1cdd67d617de04f43c23220f
tofa93 <tofig.valiyev1993@gmail.com>
2023-12-16 23:36:49 +0100

    adde few changes

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const blog = require('./models/blog');
 const app = express();
 
 const username = encodeURIComponent("tofa993");
 const password = encodeURIComponent("19932005Vvt");
 
-const dburi = `mongodb+srv://${username}:${password}@cluster0.yu23f4v.mongodb.net/?retryWrites=true&w=majority`;
+const dburi = `mongodb+srv://tofa993:19932005Vvt@cluster0.yu23f4v.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.connect(dburi, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => {
-        app.listen(3000);
-    })
-    .catch((err) => {
-        console.log(err)
-    })
+    .then(result => app.listen(3000))
+    .catch(err => console.log(err));
 
-
-// register view engine
 app.set('view engine', 'ejs');
 
 // middleware & static files(e.g images, css files etc)
 app.use(express.static('public'));
 
 app.use(morgan('dev'));
+
+// sandbox routes for just testing
+
+app.get('/add-blog', (req, res) => {
+    const newBlog = new blog({
+        title: 'New blog 2',
+        snippet: 'About my new blog',
+        body: 'Body text of the blog'
+    });
+
+    newBlog.save()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
 
 app.get("/", (req, res) => {
     const blogs = [
