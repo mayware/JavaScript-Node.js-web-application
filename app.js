8d6ebf01f29bc18e0846d7865a6387cd31d54c6b
tofa93 <tofig.valiyev1993@gmail.com>
2023-12-17 02:36:02 +0100

    added new ejs file for blod detail, dynamic id in params'

--- app.js
+++ app.js
@@ -49,6 +49,17 @@ app.post('/blogs', (req, res) => {
         })
 })
 
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id // params.id refers to this /blogs/:id
+    Blog.findById(id)
+        .then(result => {
+            res.render('details', { blog: result, title: 'Blog Details' })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create a new blog' });
 })
